c23d81bde620c1de10bf8c4ca6972ac84849cae7
culturetogo <m.mueller@culture-to-go.de>
2025-01-09 07:19:33 +0100

    fetaure: added info section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,7 @@ const loading = ref(false);
 const error = ref(null);
 const currentInput = ref('');
 const selectedId = ref(null);
+const showInfo = computed (() => !selectedId.value);
 
 async function fetchData() {
   // check for valid id input
@@ -51,7 +52,7 @@ function copyToClipboard(text) {
     <div class="controls content-element">
       <div class="search-control">
         <input type="text" class="search" v-model="currentInput" 
-          placeholder="ppn eintragen"
+          placeholder="PPN eingeben"
         />
         <button @click="fetchData()" v-if="currentInput.length">Abschicken</button>
         <img src="@/assets/icons/x-circle.svg" alt="Edit" class="icon close-icon"
@@ -60,6 +61,17 @@ function copyToClipboard(text) {
         />
       </div>
     </div>
+    <template v-if="showInfo">
+      <div class="content-element">
+
+        <p>
+          Dieses Tool ermöglicht die Formatierung bibliografischer Informationen aus dem <a href="https://opac.k10plus.de/" alt="zum K10Plus-Katalog" target="_blank">K10plus-Katalog</a>.
+          Geben Sie eine Hier die Pica-Produktions-Nummer (PPN) ein und klicken Sie auf "Abschicken", um die bibliografischen Informationen
+          für das entsprechende Werk zu erhalten. Die Informationen können als Text oder HTML kopiert werden.
+        </p>
+        <p>Die PPN steht bei Einträgen im <a href="https://opac.k10plus.de/" alt="zum K10Plus-Katalog" target="_blank">OPAC von K10plus</a>im ersten Feld.</p>
+      </div>
+    </template>
     <template  v-if="citations">
       <div class="display content-element">
         <div>
